73ebf8e5a97422f96bea797ac67a58dfccb8cc7b
AsenDulakshanaKavinda <s22010362@ousl.lk>
2026-06-23 21:26:37 +0530

    add data processing class

--- prediction_model/data_pipeline/data_ingesion.py
+++ prediction_model/data_pipeline/data_ingesion.py
@@ -1,3 +1,16 @@
+import pandas as pd
+from pandas import DataFrame
+from sqlalchemy import Engine
+
+from utils import handle_errors, log
+
+
 class DataIngesion:
-    def __init__(self):
-        pass
+    def __init__(self, query: str, engine: Engine):
+        self.query = query
+        self.engine = engine
+
+    @handle_errors("load dataset", 20001)
+    def load_dataset(self) -> DataFrame:
+        log.info("Loading dataset from the database")
+        return pd.read_sql(self.query, self.engine)
